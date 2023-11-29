d25c584be9f182cd300d34842a57247ac5163108
Alejo-fv <alflorezve@unal.edu.co>
2023-11-29 00:49:36 -0500

    Commit

---
+++ ejercicio8-2/src/ejercicio8/pkg2/Ejercicio82.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio8.pkg2;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Ejercicio82 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        panel iniciar = new panel();
+        
+        iniciar.setVisible(true); 
+    }
+    
+}
